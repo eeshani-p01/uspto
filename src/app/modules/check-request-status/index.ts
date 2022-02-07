0f2cafa18dd963103670092af1034853a927d22a
223030126 <eeshani.p01@gmail.com>
2022-02-08 00:17:56 +0530

    Added all the navigation components

---
+++ src/app/modules/check-request-status/index.ts
@@ -0,0 +1,1 @@
+export * from './check-request-status.component';
---
+++ src/app/modules/connect-data/connect-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connect-data',
+  templateUrl: './connect-data.component.html',
+  styleUrls: ['./connect-data.component.scss']
+})
+export class ConnectDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

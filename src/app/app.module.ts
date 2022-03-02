3ce41e07486bfecf8ecd50a9ccfc34175415283e
223030126 <eeshani.p01@gmail.com>
2022-03-02 16:17:30 +0530

    added ng2 semantic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {SuiModule} from 'ng2-semantic-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +27,9 @@ import {
   imports: [
     BrowserModule,
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SuiModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
